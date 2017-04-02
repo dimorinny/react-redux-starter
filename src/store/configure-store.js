6251806f27661415add40f676b5f6971aea30865
dmerkurev <dmerkurev@avito.ru>
2017-04-02 13:53:31 +0300

    update dependencies

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware, compose} from 'redux';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import {browserHistory} from 'react-router';
 import {routerMiddleware} from 'react-router-redux';
 import promiseMiddleware from 'redux-promise-middleware';
